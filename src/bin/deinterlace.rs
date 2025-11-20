c8ae4f3cbc1b46bf426caccba095921954c9d761
rafern <rafern@protonmail.com>
2025-11-20 19:40:06 +0000

    Deinterlacing, fixed CSV export

---
+++ src/bin/deinterlace.rs
@@ -0,0 +1,58 @@
+use std::path::PathBuf;
+
+use noita_eye_messages::data::{csv_export::export_csv_messages_or_exit, csv_import::import_csv_messages_or_exit, message::{Message, MessageList}};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    /// Stride to deinterlace with
+    stride: usize,
+    /// Path to CSV file containing message data. Use "data/all-original.csv" from the repository if you want to use the standard eye messages
+    in_data_path: std::path::PathBuf,
+    /// Path where CSV files with deinterlaced contents will be stored. A "-0" to "-3" suffix will be added to the file name if, for example, you are deinterlacing with a stride of 4
+    out_data_path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Args::parse();
+    let messages = import_csv_messages_or_exit(&args.in_data_path);
+
+    let out_data_path_osstr = std::path::absolute(args.out_data_path).expect("Expected a valid file path");
+    let out_data_path = out_data_path_osstr.as_path();
+    if out_data_path.is_dir() {
+        eprintln!("{} is a directory. Aborted", out_data_path_osstr.display());
+        std::process::exit(1);
+    }
+
+    let out_dir = out_data_path.parent().unwrap();
+    let (file_name_prefix, file_extension): (String, String) = {
+        let file_name = String::from(out_data_path.file_name().unwrap().to_str().unwrap());
+        match file_name.rfind('.') {
+            Some(idx) if idx > 0 => (String::from(&file_name[..idx]), String::from(&file_name[idx..])),
+            _ => (file_name, String::new())
+        }
+    };
+
+    for offset in 0..args.stride {
+        let mut messages_out = MessageList::default();
+
+        for message in messages.iter() {
+            let mut message_out = Message::default();
+            message_out.name = message.name.clone();
+
+            for i in (offset..message.data.len()).step_by(args.stride) {
+                message_out.data.push(message.data[i]);
+            }
+
+            if message_out.data.len() > 0 {
+                messages_out.push(message_out);
+            }
+        }
+
+        if messages_out.len() > 0 {
+            let mut out_path_deint = PathBuf::from(out_dir);
+            out_path_deint.push(format!("{file_name_prefix}-{offset}{file_extension}"));
+            export_csv_messages_or_exit(&out_path_deint, &messages_out);
+        }
+    }
+}
--- src/data/csv_export.rs
+++ src/data/csv_export.rs
@@ -5,8 +5,15 @@ use super::message::MessageList;
 
 pub fn export_csv_messages(path: &std::path::PathBuf, messages: &MessageList) -> Result<(), Box<dyn Error>> {
     let mut file = std::fs::File::create(path)?;
+    let mut first = true;
 
     for message in messages.iter() {
+        if first {
+            first = false;
+        } else {
+            file.write(b"\n")?;
+        }
+
         file.write(message.name.as_bytes())?;
         for c in message.data.iter() {
             file.write(format!(",{}", c).as_bytes())?;
