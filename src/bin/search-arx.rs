ead5e67ddf71fc514bcd8928739dcab613cf9883
rafern <rafern@protonmail.com>
2025-11-24 01:45:03 +0000

    Experimental Lua-based scripting: 225x slower Need to try alternatives, Lua performance is terrible for this purpose: - wasmtime (but compiled from which language? are we really going to have a bunch of pre-compiled blobs in the repo? if not, how are we going to compile it at runtime?) - rhai (it doesn't use bytecode, which means it's probably gonna be slower than lua) - rune (uses a stack-based VM, similar to wasm, so this looks promising) - cel, via cel-rust (a limited language for expressions, should be really fast in theory, but it also means that some ciphers probably can't be written using this)

--- src/bin/search-arx.rs
+++ src/bin/search-arx.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
-use noita_eye_messages::ciphers::arx::{ARX_ROUND_COUNT, ARXKey, decrypt_arx};
+use mlua::Lua;
+use noita_eye_messages::ciphers::arx::{ARX_ROUND_COUNT, ARXKey};
+use std::error::Error;
 use std::time::Instant;
 use noita_eye_messages::critical_section;
 use noita_eye_messages::utils::threading::{AsyncTaskList, Semaphore};
@@ -7,6 +9,9 @@ use noita_eye_messages::data::message::MessageList;
 use noita_eye_messages::utils::print::{print_message, format_big_num, MessagePrintConfig};
 use noita_eye_messages::utils::compare::{char_num, is_alphanum, is_ord, is_alpha, is_upper_alpha, is_lower_alpha, is_upper_atoi, is_lower_atoi, is_num};
 use noita_eye_messages::data::csv_import::import_csv_messages_or_exit;
+
+// TODO use mlua so that you can use custom predicates and decryption functions
+//      provided by the user as a lua script
 
 #[derive(Parser)]
 struct Args {
@@ -67,10 +72,10 @@ macro_rules! permute_key {
     };
 }
 
-fn try_key(key: &ARXKey, working_messages: &mut MessageList, messages: &MessageList, log_semaphore: &Semaphore) {
+fn try_key(key: &ARXKey, working_messages: &mut MessageList, messages: &MessageList, log_semaphore: &Semaphore, decrypt: &mlua::Function) {
     // first message special case. put conditions for repeated sections here
     let pt_msg_0 = &mut working_messages[0];
-    decrypt_arx(&messages[0], pt_msg_0, key);
+    pt_msg_0.data[0] = decrypt.call::<u8>((messages[0].data[0], [key.rounds[0].add, key.rounds[0].rotate, key.rounds[0].xor, key.rounds[1].add, key.rounds[1].rotate, key.rounds[1].xor])).unwrap();
     // if pt_msg_0.data[1] != char_num(':') { return }
     // if pt_msg_0.data[1] != char_num('.') { return }
     // if pt_msg_0.data[2] != char_num(' ') { return }
@@ -81,10 +86,10 @@ fn try_key(key: &ARXKey, working_messages: &mut MessageList, messages: &MessageL
 
     // other messages
     for m in 1..messages.len() {
-        let pt_msg = &mut working_messages[m];
-        decrypt_arx(&messages[m], pt_msg, key);
+        let pt_msg_m = &mut working_messages[m];
+        pt_msg_m.data[0] = decrypt.call::<u8>((messages[m].data[0], [key.rounds[0].add, key.rounds[0].rotate, key.rounds[0].xor, key.rounds[1].add, key.rounds[1].rotate, key.rounds[1].xor])).unwrap();
 
-        let pt_msg_m_0 = pt_msg.data[0];
+        let pt_msg_m_0 = pt_msg_m.data[0];
         // if is_alpha(pt_msg_m_0) != is_alpha(pt_msg_0_0) { return }
         // if is_upper_alpha(pt_msg_m_0) != is_upper_alpha(pt_msg_0_0) { return }
         // if is_lower_alpha(pt_msg_m_0) != is_lower_alpha(pt_msg_0_0) { return }
@@ -127,6 +132,36 @@ fn preamble(messages: &MessageList, keys_total: &mut u64) {
     println!();
 }
 
+fn setup_lua(log_semaphore: &Semaphore) -> Result<Lua, Box<dyn Error>> {
+    let lua = Lua::new();
+
+    let api_table = lua.create_table()?;
+    let log_semaphore = log_semaphore.clone();
+
+    api_table.set("print", lua.create_function(move |_, msg: String| {
+        critical_section!(log_semaphore, {
+            println!("[Lua] {}", msg);
+        });
+        Ok(())
+    })?)?;
+
+    api_table.set("mod_add_byte", lua.create_function(|_, (lhs, rhs): (u8, u8)| {
+        Ok(lhs.wrapping_add(rhs))
+    })?)?;
+
+    api_table.set("rotate_byte", lua.create_function(|_, (lhs, rhs): (u8, u32)| {
+        Ok(lhs.rotate_right(rhs))
+    })?)?;
+
+    api_table.set("xor_byte", lua.create_function(|_, (lhs, rhs): (u8, u8)| {
+        Ok(lhs ^ rhs)
+    })?)?;
+
+    lua.globals().set("eyes", api_table)?;
+
+    Ok(lua)
+}
+
 fn search_task(messages: &MessageList, worker_id: u32, worker_total: u32, keys_total: u64, log_semaphore: Semaphore) {
     let mut working_messages: MessageList = messages.clone();
     let mut key = ARXKey::default();
@@ -135,8 +170,20 @@ fn search_task(messages: &MessageList, worker_id: u32, worker_total: u32, keys_t
     let mut kps_accum_skips = 0;
     let mut worker_keys_total = keys_total;
 
+    let lua = setup_lua(&log_semaphore).unwrap();
+    lua.load(r#"
+        function decrypt(byte, key)
+            local add = eyes.mod_add_byte;
+            local rot = eyes.rotate_byte;
+            local xor = eyes.xor_byte;
+            return xor(rot(add(xor(rot(add(byte, key[1]), key[2]), key[3]), key[4]), key[5]), key[6])
+        end
+    "#).exec().unwrap();
+
+    let decrypt = lua.globals().get::<mlua::Function>("decrypt").unwrap();
+
     permute_key!(worker_id, worker_total, worker_keys_total, key, {
-        try_key(&key, &mut working_messages, messages, &log_semaphore);
+        try_key(&key, &mut working_messages, messages, &log_semaphore, &decrypt);
 
         keys_checked += 1;
         // XXX this makes the last round *look* like it's not changing in the
--- src/ciphers/arx.rs
+++ src/ciphers/arx.rs
@@ -1,13 +1,10 @@
-use crate::data::message::Message;
-
-const ARX_ORDER: i32 = 1; // ARX, RAX, XAR, AXR, RXA, XRA
 pub const ARX_ROUND_COUNT: usize = 2;
 
 #[derive(Debug)]
 #[derive(Default)]
 pub struct ARXRound {
     /** range: 0-7. u32 instead of u8 for performance reasons */
-    pub rotate: u32,
+    pub rotate: u8,
     /** range: 0-255 */
     pub add: u8,
     /** range: 0-255 */
@@ -18,53 +15,4 @@ pub struct ARXRound {
 #[derive(Default)]
 pub struct ARXKey {
     pub rounds: [ARXRound; ARX_ROUND_COUNT],
-}
-
-pub fn apply_arx_round(in_byte: u8, round: &ARXRound) -> u8 {
-    let mut byte: u8 = in_byte;
-    match ARX_ORDER {
-        0 => {
-            byte = byte.rotate_right(round.rotate);
-            byte = byte.wrapping_add(round.add);
-            byte ^ round.xor
-        },
-        1 => {
-            byte = byte.wrapping_add(round.add);
-            byte = byte.rotate_right(round.rotate);
-            byte ^ round.xor
-        },
-        2 => {
-            byte ^= round.xor;
-            byte = byte.rotate_right(round.rotate);
-            byte.wrapping_add(round.add)
-        },
-        3 => {
-            byte = byte.rotate_right(round.rotate);
-            byte ^= round.xor;
-            byte.wrapping_add(round.add)
-        },
-        4 => {
-            byte = byte.wrapping_add(round.add);
-            byte ^= round.xor;
-            byte.rotate_right(round.rotate)
-        },
-        _ => {
-            byte ^= round.xor;
-            byte = byte.wrapping_add(round.add);
-            byte.rotate_right(round.rotate)
-        }
-    }
-}
-
-pub fn decrypt_arx(ct_msg: &Message, pt_msg: &mut Message, key: &ARXKey) {
-    // HACK only decrypting first char to get candidates for A-I, a-i or 0-9
-    for i in 0..1/*ct_msg.data_len*/ {
-        let mut byte = ct_msg.data[i];
-
-        for round in &key.rounds {
-            byte = apply_arx_round(byte, round);
-        }
-
-        pt_msg.data[i] = byte;
-    }
 }
